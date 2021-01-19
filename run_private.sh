af6cba21a05f5e047b875a648b73d297eb310dff
Tony <b05202068@ntu.edu.tw>
2021-01-20 01:08:23 +0800

    script to run private input with timeout = 86400/78

--- run_private.sh
+++ run_private.sh
@@ -1,7 +1,9 @@
-INPUT_DIR=../testcase
+#! /bin/bash
+# should be placed under or-tool 
+INPUT_DIR=../ada-private-cases/release
 make build SOURCE=../ada/ada_challenge.cc
 #some flags
-INPUTNAME=--inputname
+#INPUTNAME=--inputname
 T=--T
 TIMEOUT=--timeout
 SORT_JOB=--sortjob
@@ -10,12 +12,24 @@ SAVE=--save
 LOAD=--load
 TOPO=--toposort
 VERBOSE=--verbose
-./bin/ada_challenge $INPUTNAME 00 $T 96000 < $INPUT_DIR/00.in
-./bin/ada_challenge $INPUTNAME 01 $T 96000 < $INPUT_DIR/01.in
-./bin/ada_challenge $INPUTNAME 02 $T 96000 < $INPUT_DIR/02.in 
-./bin/ada_challenge $INPUTNAME 03 $T 96000 < $INPUT_DIR/03.in
-./bin/ada_challenge $INPUTNAME 04 $T 96000 < $INPUT_DIR/04.in
-./bin/ada_challenge $INPUTNAME 05 $T 96000 < $INPUT_DIR/05.in
-./bin/ada_challenge $INPUTNAME 06 $T 96000 < $INPUT_DIR/06.in
-./bin/ada_challenge $INPUTNAME 07 $T 96000 < $INPUT_DIR/07.in
-./bin/ada_challenge $INPUTNAME 08 $T 96000 < $INPUT_DIR/08.in
+
+cnt=0
+t=96000
+timeout=78*3600/78 #can be set to $1
+for input in `ls $INPUT_DIR`; do
+    #echo $input
+    echo $cnt
+    ./bin/ada_challenge $INPUTNAME $cnt $T $t $TIMEOUT $timeout $VERBOSE < "$INPUT_DIR/$input" > "$cnt.out"
+    cnt=$((cnt+1))
+done
+
+
+#./bin/ada_challenge $INPUTNAME 00 $T 96000 < $INPUT_DIR/00.in
+#./bin/ada_challenge $INPUTNAME 01 $T 96000 < $INPUT_DIR/01.in
+#./bin/ada_challenge $INPUTNAME 02 $T 96000 < $INPUT_DIR/02.in 
+#./bin/ada_challenge $INPUTNAME 03 $T 96000 < $INPUT_DIR/03.in
+#./bin/ada_challenge $INPUTNAME 04 $T 96000 < $INPUT_DIR/04.in
+#./bin/ada_challenge $INPUTNAME 05 $T 96000 < $INPUT_DIR/05.in
+#./bin/ada_challenge $INPUTNAME 06 $T 96000 < $INPUT_DIR/06.in
+#./bin/ada_challenge $INPUTNAME 07 $T 96000 < $INPUT_DIR/07.in
+#./bin/ada_challenge $INPUTNAME 08 $T 96000 < $INPUT_DIR/08.in

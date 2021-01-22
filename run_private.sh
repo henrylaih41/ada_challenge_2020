08d938773b82bc6fa0be0f0c7f2e5d0746d83c16
henrylaih41 <henrylaih41@gmail.com>
2021-01-22 17:38:17 +0800

    change runtime

--- run_private.sh
+++ run_private.sh
@@ -10,6 +10,6 @@ mkdir ./Team09_private_output
 for input in $(ls $INPUT_DIR); do
     filename=$(echo $input | cut -f 1 -d '.')
     ### the --inputname is actually --outputname
-    ./bin/ada_challenge --inputname "$OUTPUT_DIR/$filename.hint" --T $timeHor --timeout $timeOut < "$INPUT_DIR/$input"
+    ./bin/ada_challenge --inputname "$OUTPUT_DIR/$filename.out" --T $timeHor --timeout $timeOut --verbose < "$INPUT_DIR/$input"
     echo "$input Done"
 done
